db78a5b6023f1c318056310db48cb9e3f4032dac
Alex_laptop_ubuntu <alexandre.mayer@epfl.ch>
2026-09-03 16:06:23 +0200

    Replaced sqrt+**2 with abs

--- src/thbsplines/fenicsx/adaptivity.py
+++ src/thbsplines/fenicsx/adaptivity.py
@@ -8,8 +8,8 @@ def dorfler_marking(hierarchical_space: HierarchicalSpace, theta: float,
     local_error_vector = dolfinx_fem.assemble_vector(local_error_form)
     local_error_vector.scatter_forward()
 
-    cell_errors = np.sqrt(local_error_vector.array)
-    squared_errors = cell_errors**2
+    # cell_errors = np.sqrt(local_error_vector.array)
+    squared_errors = np.abs(local_error_vector.array)# cell_errors**2
     total_squared_error = np.sum(squared_errors)
     descending_indices = np.flip(np.argsort(squared_errors))
     sorted_squared_errors = squared_errors[descending_indices]
@@ -17,7 +17,7 @@ def dorfler_marking(hierarchical_space: HierarchicalSpace, theta: float,
     threshold_value = theta*total_squared_error
     num_cells_to_mark = max(2, np.searchsorted(cumulative_errors, threshold_value)+1)
     top_error_indices = descending_indices[:num_cells_to_mark]
-    print(f"Total cells marked via Dörfler (theta={theta}): {num_cells_to_mark} out of {len(cell_errors)}")
+    print(f"Total cells marked via Dörfler (theta={theta}): {num_cells_to_mark} out of {len(squared_errors)}")
     print(f"Indices to refine: {top_error_indices[:10]}")
     
     my_arr = []
