4c36e247597bdd7e7498a873419e61616657293c
Alex_laptop_ubuntu <alexandre.mayer@epfl.ch>
2026-09-04 14:13:37 +0200

    Simplified the code to directly create the linear/bilinear forms without user facing raw pointers manipulations. This still needs some work to eliminate the manipulation of dolfinx.fem.form .

--- src/thbsplines/fenicsx/kernels.py
+++ src/thbsplines/fenicsx/kernels.py
@@ -1,16 +1,22 @@
 import numpy as np
 import numba
-
+from dolfinx.jit import ffcx_jit
 import cffi
-
+from dolfinx import default_real_type, default_scalar_type
+rtype = default_real_type
+dtype = default_scalar_type
 from ffcx.codegeneration.utils import (
     empty_void_pointer,
     numba_ufcx_kernel_signature as ufcx_signature,
 )
 
-def make_bilinear_kernel(dtype, rtype, ufcx_kernel,
-                          padded_dofs, local_dofs):
+def _make_kernel(mesh, ufl_form):
+    ufcx, _, _ = ffcx_jit(mesh.comm, ufl_form, form_compiler_options={"scalar_type": dtype})  # type: ignore
+    return getattr(ufcx.form_integrals[0], f"tabulate_tensor_{np.dtype(dtype).name}")  # type: ignore
+
+def make_bilinear_kernel(mesh, ufl_form, padded_dofs, local_dofs):
     ffi = cffi.FFI()
+    ufcx_kernel = _make_kernel(mesh, ufl_form)
     
     @numba.cfunc(ufcx_signature(dtype, rtype), nopython=True)
     def tabulate_A(
@@ -37,8 +43,8 @@ def make_bilinear_kernel(dtype, rtype, ufcx_kernel,
         A[:, :] = G @ A0 @ G.T
     return tabulate_A
 
-def make_linear_kernel(dtype, rtype, ufcx_kernel,
-                          padded_dofs, local_dofs):
+def make_linear_kernel(mesh, ufl_form, padded_dofs, local_dofs):
+    ufcx_kernel = _make_kernel(mesh, ufl_form)
     ffi = cffi.FFI()
 
     @numba.cfunc(ufcx_signature(dtype, rtype), nopython=True)
@@ -89,7 +95,8 @@ def _lift_vector_operator(G):
 
     return G_vec
 
-def make_vector_bilinear_kernel(dtype, rtype, ufcx_kernel, padded_dofs, local_dofs):
+def make_vector_bilinear_kernel(mesh, ufl_form, padded_dofs, local_dofs):
+    ufcx_kernel = _make_kernel(mesh, ufl_form)
     ffi = cffi.FFI()
     local_dofs_vec = 2*local_dofs
     padded_dofs_vec = 2*padded_dofs
@@ -114,7 +121,8 @@ def make_vector_bilinear_kernel(dtype, rtype, ufcx_kernel, padded_dofs, local_do
 
     return tabulate_A
 
-def make_vector_linear_kernel(dtype, rtype, ufcx_kernel, padded_dofs, local_dofs):
+def make_vector_linear_kernel(mesh, ufl_form, padded_dofs, local_dofs):
+    ufcx_kernel = _make_kernel(mesh, ufl_form)
     ffi = cffi.FFI()
     local_dofs_vec = 2*local_dofs
     padded_dofs_vec = 2*padded_dofs
